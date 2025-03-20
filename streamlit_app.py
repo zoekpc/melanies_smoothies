c7d63c18c2fa59906254dd88b9ee6acad66b0fb6
zoekpc <zoe.martinez@kpconsulting.fr>
2025-03-20 12:44:26 +0000

    transfer response into dataframe (no query)

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,5 @@ if ingredients_list:
         st.success(strSuccess, icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
