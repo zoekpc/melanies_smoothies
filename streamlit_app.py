ba94bd542d84ad47c886382efb535c0f2fa78606
zoekpc <zoe.martinez@kpconsulting.fr>
2025-03-20 12:46:38 +0000

    into the if

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list : 
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -52,6 +54,4 @@ if ingredients_list:
         strSuccess = "Your smoothie is ordered, " + name_on_order
         st.success(strSuccess, icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
