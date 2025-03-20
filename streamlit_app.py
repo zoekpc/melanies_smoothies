9e074c9d391e1f341ab068aa503ac26d0aa67952
zoekpc <zoe.martinez@kpconsulting.fr>
2025-03-20 11:22:24 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,30 @@ st.write('The name on your smoothie will be :', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("INTERNATIONAL_CURRENCIES.public.fruit_options").select(col('CURRENCIES'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients : ',
+    my_dataframe,
+    max_selections=5
+)
+
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list : 
+        ingredients_string += fruit_chosen + ' '
+
+    #st.write(ingredients_string)
+
+    my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order) 
+        values ('""" + ingredients_string +"""','""" + name_on_order + """')"""
+    #st.write(my_insert_stmt)
+
+    time_to_insert = st.button('Submit order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        strSuccess = "Your smoothie is ordered, " + name_on_order
+        st.success(strSuccess, icon="✅")
